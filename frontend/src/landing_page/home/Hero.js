c4098da9fbe2677d59f2763489260cc143a606d5
HarshSingh6666 <harshkumarsingh23272598@gmail.com>
2025-09-04 13:37:52 +0530

    Update Hero.js

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Hero() {
     return ( 
-        <div className='container p-5 mb-5'>
-            <div className='row text-center'>
-                <img src='media/homeHero.png' alt='Image' className='mb-5'/>
-                <h1 className='mb-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <Link to="/Signup">
-                    <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>
-                        Sign up for free
-                    </button>
-                </Link>
+        <section className='container p-5 mb-5'>
+            <div className='row justify-content-center text-center'>
+                <div className='col-lg-8'>
+                    <img src='media/homeHero.png' alt='Hero showing investment opportunities' className='mb-5 img-fluid'/>
+                    <h1 className='mb-4'>Invest in everything</h1>
+                    <p className='mb-4'>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                    <Link to="/Signup">
+                        <button className='btn btn-primary fs-5 px-4 py-2 w-100 w-md-50 w-lg-25 mx-auto'>
+                            Sign up for free
+                        </button>
+                    </Link>
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
 
 export default Hero;
+
